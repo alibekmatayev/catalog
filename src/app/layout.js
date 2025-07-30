0f8366eb9247f97cb8fdaf2fe051d02707825226
amatayev <alibekm48@gmail.com>
2025-07-30 15:22:25 +0500

    Add: styles; Fix: components structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Montserrat } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./globals.scss";
+import "@/app/styles/main.scss";
 const montserrat = Montserrat({ subsets: ["latin", "cyrillic"] });
 
 export const metadata = {
