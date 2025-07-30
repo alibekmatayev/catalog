7fbd52313881ff3497c9feae6453333a311b8651
amatayev <alibekm48@gmail.com>
2025-07-30 14:46:04 +0500

    Add: added components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Добро пожаловать в каталог товаров</h1>
+      <p>Здесь будут товары, фильтры, корзина и многое другое.</p>
+    </div>
+  );
+}
